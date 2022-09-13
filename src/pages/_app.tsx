4614cfa90d6b51acdb7cbe95dfcb6689a47a5a08
Curtis Oyakoya <curtsoyaks@gmail.com>
2022-09-13 22:22:48 +0100

    update to dynamic import

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,8 +5,11 @@ import { ChakraProvider } from "@chakra-ui/react";
 import Script from "next/script";
 import { useRouter } from "next/router";
 import theme from "../theme/theme";
-import AnimatedCursor from "react-animated-cursor"
+import dynamic from "next/dynamic";
 
+const AnimatedCursor = dynamic(() => import("react-animated-cursor"), {
+  ssr: false,
+});
 function MyApp({ Component, pageProps }: AppProps) {
   const router = useRouter();
   const [loading, setLoading] = useState(false);
@@ -34,7 +37,7 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme} resetCSS>
       <Component {...pageProps} />
-      <AnimatedCursor/>
+      <AnimatedCursor />
       <Script id="tawk" strategy="lazyOnload">
         {`
       var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
